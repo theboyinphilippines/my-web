25e8c636636581cb44635dea19c65474c8101aab
qianlibeishang <64119152+theboyinphilippines@users.noreply.github.com>
2026-09-24 22:05:15 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theboyinphilippines.github.io',
-  base: '',
+  site: 'https://myblog.github.io',
 });
