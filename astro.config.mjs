168f02685c09193918cc142c5eae34187cbb3462
qianlibeishang <64119152+theboyinphilippines@users.noreply.github.com>
2026-09-24 21:40:57 +0800

    Remove trailing slash from site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theboyinphilippines.github.io/',
+  site: 'https://theboyinphilippines.github.io',
   base: '/my-web',
 });
