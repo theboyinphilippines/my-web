daa23ecfca0f5e510afba91354603a1969c0710e
qianlibeishang <64119152+theboyinphilippines@users.noreply.github.com>
2026-09-24 21:38:41 +0800

    Configure site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://theboyinphilippines.github.io/',
+  base: '/my-web',
+});
