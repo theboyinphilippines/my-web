cb972adc8c5a58a30464a553e14379f0e8733bba
qianlibeishang <15173191396@163.com>
2026-09-25 10:15:27 +0800

    modify to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
-output: 'server',
+output: 'static',
   adapter: cloudflare(),
 });
